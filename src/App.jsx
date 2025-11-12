8e5569de2bb67bb43e7026e3e89f6d6a99d240db
Wealthometer <wealthhajoh87@gmail.com>
2025-11-12 11:07:06 +0100

    Create App.jsx
    Co-Authored-By: Vanessa <216306524+adorableprincessvee@users.noreply.github.com>
    Co-Authored-By: ASTROL360 <212729570+ASTROL360@users.noreply.github.com>
    Co-Authored-By: Agbaje Stephen <agbajestephen5@gmail.com>
    Co-Authored-By: Ayothaplug <188990461+Ayothaplug@users.noreply.github.com>
    Co-Authored-By: Imamyunus28 <234481158+Imamyunus28@users.noreply.github.com>
    Co-Authored-By: Nanribeth1 <171723338+Nanribeth1@users.noreply.github.com>
    Co-Authored-By: Ssans124 <234150813+Ssans124@users.noreply.github.com>
    Co-Authored-By: agoroibrahim123-bot <233746404+agoroibrahim123-bot@users.noreply.github.com>
    Co-Authored-By: Davera <210092247+nxora@users.noreply.github.com>
    Co-Authored-By: oluchukwu <184862563+olu26@users.noreply.github.com>
    Co-Authored-By: oromena97 <127771106+oromena97@users.noreply.github.com>
    Co-Authored-By: poiselotus <179840919+poiselotus@users.noreply.github.com>
    Co-Authored-By: serikisultan29-hub <233746030+serikisultan29-hub@users.noreply.github.com>
    Co-Authored-By: zaneXXL203 <198445062+zaneXXL203@users.noreply.github.com>

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ import './App.css'
+ import Layout from './Layout'
+import { BrowserRouter } from "react-router-dom";
+ 
+
+function App() {
+ 
+  return (
+    <>
+    <BrowserRouter>
+     <Layout/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
