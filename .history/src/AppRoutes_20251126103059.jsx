541febf7290ecfa489e8d31ebb9ec6808d3554d9
Vanessa011 <adorableprincessvee@gmail.com>
2025-11-26 10:34:57 +0100

    updated

---
+++ .history/src/AppRoutes_20251126103059.jsx
@@ -0,0 +1,53 @@
+import { Routes, Route } from "react-router-dom";
+import Mailbox1 from "./MailBox1";
+import Mailboxes from "./Mailboxes";
+import Companies from "./Companies";
+import LoginPage from "./pages/LoginPage";
+import ManualRespond from "./ManualRespond";
+import ForgotPassword from "./pages/ForgotPassword";
+import EmailDashboard from "./components/EmailDashboard";
+<<<<<<< HEAD
+import CouldNotRespond from "./CouldNotRespond";
+import Companyview2 from "./Companyview2";
+=======
+import LoginPage from "./pages/LoginPage";
+import Companyview2 from "./companyview2";
+import ForgotPassword from "./pages/forgotPassword";
+import CouldNotRespond from "./couldNotRespond";
+import UserListContent from "./UserList";
+>>>>>>> master
+
+// import Mailbox2 from "./Mailbox2";
+// import Users from "./Users";
+// … your other pages
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      <Route path="/mailbox1" element={<Mailbox1 />} />
+      <Route path="/manualrespond" element={<ManualRespond/>} />
+      {/* <Route path="/mailbox1" element={<Maunally_resp/>}/> */}
+
+      <Route path="/mailboxes" element={<Mailboxes />} />
+      <Route path="/companies" element={<Companies />} />
+      <Route path="/email" element={<EmailDashboard />} />
+      <Route path="/couldnotrespond" element={<CouldNotRespond/>}/>
+      <Route path="/email" element={<Companyview2 />} />
+      <Route path="/users" element={<UserListContent/>} />
+
+      {/* <Route path="/mailbox2" element={<Mailbox2 />} /> 
+
+<<<<<<< HEAD
+      <Route path="/users" element={<Users />} />
+
+=======
+      
+      <Route path="/companies" element={<Company />} />
+>>>>>>> master
+      <Route path="/blocked" element={<Blocked />} /> */}
+    </Routes>
+  );
+}
---
+++ .history/src/App_20251126103059.jsx
@@ -0,0 +1,25 @@
+ import './App.css'
+ import Layout from './Layout'
+import { BrowserRouter } from "react-router-dom";
+import CompanyList from "./components/CompanyList";
+import AppRoutes from './AppRoutes';
+ 
+
+function App() {
+ 
+  return (
+    <>
+    <BrowserRouter>
+      {/* <AppRoutes/> */}
+<<<<<<< HEAD
+      <Layout/>
+=======
+     <Layout/>
+>>>>>>> master
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+  
